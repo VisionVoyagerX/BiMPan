9048113fcc22f26970e0297a665f7689acad9793
VisionVoyagerX <nickd_96@hotmail.com>
2024-06-24 15:45:11 +0300

    Update inference.py

--- inference.py
+++ inference.py
@@ -7,109 +7,60 @@ from torch.nn import MSELoss
 from torch.utils.data import DataLoader
 from torchvision.transforms import Resize, RandomHorizontalFlip, RandomVerticalFlip, RandomRotation
 from torchmetrics import MetricCollection, PeakSignalNoiseRatio, StructuralSimilarityIndexMeasure
-from torchmetrics.image import SpectralAngleMapper, ErrorRelativeGlobalDimensionlessSynthesis
-
 from torch.optim.lr_scheduler import StepLR
-import matplotlib.pyplot as plt
+#from torchsummary import summary
+
 from data_loader.DataLoader import DIV2K, GaoFen2, Sev2Mod, WV3, GaoFen2panformer
 from BiMPan import BiMPan
 from utils import *
-import time
 import numpy as np
-
-
-def measure_gpu_throughput(model, input1_, input2_):
-    input1 = input1_.to('cuda')
-    input2 = input2_.to('cuda')
-    model = model.to('cuda')
-
-    ave_forward_throughput = []
-
-    ave_start = time.time()
-    for t in range(300):
-        start = time.time()
-        x = model(input1, input2)
-        end = time.time()
-        fwd_throughput = 1/(end-start)
-        # print('forward_throughput is {:.4f}'.format(fwd_throughput))
-        ave_forward_throughput.append(fwd_throughput)
-
-    ave_fwd_throughput = np.mean(ave_forward_throughput[1:])
-
-    print('Mean throughput over 300 runs: {:.4f}'.format(ave_fwd_throughput))
-
-
-def measure_gpu_latency(model, input1_, input2_):
-    input1 = input1_.to('cuda')
-    input2 = input2_.to('cuda')
-    model = model.to('cuda')
-
-    repetitions = 300
-
-    # GPU-WARM-UP
-    for _ in range(20):  # Increase warm-up iterations to ensure GPU is fully warmed up
-        _ = model(input1, input2)
-        torch.cuda.synchronize()  # Ensure the warm-up operation completes
-
-    # Measure performance
-    timings = []
-    with torch.no_grad():
-        for _ in range(repetitions):
-            start = time.time()
-            _ = model(input1, input2)
-            torch.cuda.synchronize()  # Ensure the operation completes
-            end = time.time()
-            latency = end - start
-            timings.append(latency)
-
-    mean_latency = np.mean(timings)
-    print(
-        f"Mean time over {repetitions} runs: {mean_latency} seconds")
-    return mean_latency
+import matplotlib.pyplot as plt
 
 
 def main():
-    # Prepare device
-    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    device = "cpu"
-    print(device)
-
     choose_dataset = 'WV3'  # choose dataset
 
     if choose_dataset == 'GaoFen2':
         dataset = eval('GaoFen2')
-        tr_dir = 'data/pansharpenning_dataset/GF2/train/train_gf2.h5'
-        eval_dir = 'data/pansharpenning_dataset/GF2/val/valid_gf2.h5'
-        test_dir = 'data/pansharpenning_dataset/GF2/test/test_gf2_multiExm1.h5'
+        tr_dir = '../pansharpenning_dataset/GF2/train/train_gf2.h5'
+        eval_dir = '../pansharpenning_dataset/GF2/val/valid_gf2.h5'
+        test_dir = '../pansharpenning_dataset/GF2/test/test_gf2_multiExm1.h5'
         checkpoint_dir = 'checkpoints/BiMPan_GF2/BiMPan_GF2_2024_02_18-12_21_11.pth.tar'
         ms_channel = 4
         ergas_l = 4
     elif choose_dataset == 'WV3':
         dataset = eval('WV3')
-        tr_dir = 'data/pansharpenning_dataset/WV3/train/train_wv3.h5'
-        eval_dir = 'data/pansharpenning_dataset/WV3/val/valid_wv3.h5'
-        test_dir = 'data/pansharpenning_dataset/WV3/test/test_wv3_multiExm1.h5'
+        tr_dir = '../pansharpenning_dataset/WV3/train/train_wv3.h5'
+        eval_dir = '../pansharpenning_dataset/WV3/val/valid_wv3.h5'
+        test_dir = '../pansharpenning_dataset/WV3/test/test_wv3_multiExm1.h5'
         checkpoint_dir = 'checkpoints/BiMPan_WV3/BiMPan_WV3_2024_02_17-14_16_14.pth.tar'
         ms_channel = 8
         ergas_l = 4
     else:
         print(choose_dataset, ' does not exist')
 
+
+    # Prepare device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
+
     # Initialize DataLoader
     train_dataset = dataset(
         Path(tr_dir), transforms=[(RandomHorizontalFlip(1), 0.3), (RandomVerticalFlip(1), 0.3)])  # /home/ubuntu/project
     train_loader = DataLoader(
-        dataset=train_dataset, batch_size=128, shuffle=True, drop_last=True)
-
-    # validation_dataset = dataset(
-    #   Path(eval_dir))
-    # validation_loader = DataLoader(
-    #    dataset=validation_dataset, batch_size=64, shuffle=True)
-
+        dataset=train_dataset, batch_size=32, shuffle=True, drop_last=True)
+    """
+    validation_dataset = WV3(
+        Path("/home/nick/VScode-workspace/Pansharpening_datasets/WV3_Dataset/val/valid_wv3.h5"))
+    validation_loader = DataLoader(
+        dataset=validation_dataset, batch_size=1, shuffle=True)
+    """
     test_dataset = dataset(
         Path(test_dir))
     test_loader = DataLoader(
         dataset=test_dataset, batch_size=1, shuffle=False)
+
+    # Initialize Model, optimizer, criterion and metrics
 
     # Initialize Model, optimizer, criterion and metrics
     model = BiMPan(ms_channels=ms_channel, mslr_mean=train_dataset.mslr_mean.to(device), mslr_std=train_dataset.mslr_std.to(device), pan_mean=train_dataset.pan_mean.to(device),
